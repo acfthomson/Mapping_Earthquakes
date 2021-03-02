ac66170567d34e3b7e4a09317a29ff796c921954
Amanda Thomson <acfthomson@gmail.com>
2021-03-02 15:06:53 -0500

    adding Mapping_Multiple_Points files

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,36 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+    center: [
+      34.0522, -118.2437
+    ],
+    zoom: 14
+  });
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius: city.population/100000
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+ .addTo(map);
+});
+
+// Create the tile layer that will be the background of the map
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+// Then we add the 'graymap' tile layer to the map
+streets.addTo(map);
