f777e515b114764e3f3003e4313aac6b0515d6e3
Amanda Thomson <acfthomson@gmail.com>
2021-03-03 09:51:58 -0500

    fixed Mapping_Geo_JSON_Linestrings logic.js

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -3,14 +3,16 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/light-v10',
+    tileSize: 512,
+    zoomOffset: -1,
     accessToken: API_KEY
 });
 
-// Create the tile layer that will be the background of the map
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+// Create a dark tile layer
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox://styles/mapbox/dark-v10'
     accessToken: API_KEY
 });
 
@@ -20,28 +22,39 @@ let baseMaps = {
   Dark: dark
 };
 
-// Create the map object with center, zoom level, and default layer
+// Create the map object with center, zoom level and default layer
 let map = L.map('mapid', {
   center: [44.0, -80.0],
   zoom: 2,
   layers: [streets]
 });
 
-// Accessing the Toronto airline routes GeoJSON URL
-let torontoData = 'https://github.com/acfthomson/Mapping_Earthquakes/blob/main/torontoRoutes.json';
+// Pass map layers into our layers control and add the layers control to the map
+L.control.layers(baseMaps).addTo(map);
+
+// Then we add the 'graymap' tile layer to the map
+streets.addTo(map);
+
+// Accessing Toronto airline routes GeoJSON URL
+let torontoData = "https://raw.githubusercontent.com/acfthomson/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Create a style for the lines
+let myStyle = {
+  color: '#ffffa1',
+  weight: 2
+};
 
 // Grab GeoJSON data
 d3.json(torontoData).then(function(data) {
-    console.log(data);
+  console.log(data);
 
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data, {
-  color: 'ffffa1',
-  weight: 2,
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup('<h3> Airline: ' + feature.properties.airline + '</h3> <hr><h3> Destination:'
-    + feature.properties.dst + '</h3>');
-  }
-})
+  // Create a GeoJSON layer with retrieved data
+  L.geoJson(data, {
+    style: myStyle,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup('<h3> Airline: ' + feature.properties.airline + '</h3> <hr><h3> Destination: '
+      + feature.properties.dst + '</h3>');
+    }
+  })
 .addTo(map);
 });
